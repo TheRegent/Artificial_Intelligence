185af7c11bc6763f5f84392f28fc0fcf1b5e5702
TheRegent <kobilinskiy.fedir@gmail.com>
2022-09-23 14:26:35 +0300

    final version

--- lab1.py
+++ lab1.py
@@ -2,15 +2,34 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import networkx as nx
+import random
 
-def print_hi(N):
-    G = nx.grid_2d_graph(N, N)
-    pos = dict((n, n) for n in G.nodes())
-    labels = dict(((i, j), i + (N - 1 - j) * N) for i, j in G.nodes())
-    nx.draw_networkx(G, pos=pos, labels=labels, with_labels=False, node_size=10)
-    plt.show()
+def ways_draw(delete, N=5):
+    exeption = (N-1)**2
 
+    if delete > exeption:
+        print("Ви ввели число більше допустимого! Граф не буде зв\'язвним.")
+    else:
+        G = nx.grid_2d_graph(N, N)
+        pos = dict((n, n) for n in G.nodes())
+        labels = dict(((i, j), i + (N - 1 - j) * N) for i, j in G.nodes())
+        nx.draw_networkx(G, pos=pos, labels=labels, with_labels=False, node_size=300, node_color="PaleGreen")
+        plt.show()
+
+        while delete > 0:
+            edges = list(G.edges)
+            chosen_edge = random.choice(edges)
+            G.remove_edge(chosen_edge[0], chosen_edge[1])
+
+            if nx.is_connected(G):
+                delete -= 1
+            else:
+                G.add_edge(chosen_edge[0], chosen_edge[1])
+        nx.draw_networkx(G, pos=pos, labels=labels, with_labels=False, node_size=300, node_color="PaleGreen")
+        plt.show()
 
 if __name__ == '__main__':
-    print_hi(5)
+    N = int(input("Введіть розмірність матриці: "))
+    delete = int(input("Введіть кількість ребер на видалення: "))
+    ways_draw(delete, N)
 
